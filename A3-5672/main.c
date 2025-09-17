a9363d87cc4b370a59782b9acd4eed86a638a437
Elrond <80281200@qq.com>
2025-09-17 17:24:23 +0800

    Update my homework

---
+++ A3-5672/main.c
@@ -0,0 +1,32 @@
+/*编程输出下面图形。其中上半层的高度（即最大的数字）由键盘输入，不超过9。*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int n,i,k,j;
+	printf("请输入一个不大于9的整数:");
+	scanf("%d", &n);
+	for (i = 0; i < n ; i++)
+	{
+		for (j = n - i- 1; j >= 0; j--)
+		{
+			printf(" ");
+		}
+		for (k = 0; k < i + 1; k++)
+		{
+			printf("%-2d", i + 1);
+		}
+		printf("\n");
+	}
+	for (i = n - 1; i > 0; i--)
+	{
+		for (j = 0; j <= n - i; j++)
+			printf(" ");
+		for (k = i; k > 0; k--)
+		{
+			printf("%-2d", i);
+		}
+		printf("\n");
+	}
+	return 0;
+}
